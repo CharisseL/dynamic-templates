7a46a4bf6772e776d16787660cf9cea603575a03
Charisse Lambert <charisse8@gmail.com>
2014-07-30 07:45:10 -0500

    checkers redo

---
+++ app/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res){
+  res.render('home.ejs');
+});
+
+app.get('/checkers', function (req, res){
+  res.render('checkers.ejs');
+});
+
+var port = process.env.PORT;
+
+app.listen(port, function(){
+  console.log('Express is now listening on PORT:', port);
+});
+
+
